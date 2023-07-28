29eb3ab66a61d529621b779716ec0544a81fb40b
Brian Seeders <brian.seeders@elastic.co>
2023-07-28 17:36:15 -0400

    Need some cds

--- .buildkite/scripts/lucene-snapshot/build-snapshot.sh
+++ .buildkite/scripts/lucene-snapshot/build-snapshot.sh
@@ -7,6 +7,7 @@ echo --- Building Lucene snapshot
 # TODO custom branches
 cd ..
 git clone git@github.com:apache/lucene.git --branch main --single-branch --depth 1
+cd lucene
 
 REVISION=$(git rev-parse --short HEAD)
 echo "Lucene Revision: $REVISION"
@@ -16,6 +17,8 @@ echo "Lucene Revision: $REVISION"
 aws s3 sync build/maven-local/ "s3://download.elasticsearch.org/lucenesnapshots/$REVISION/" --acl public-read
 
 buildkite-agent meta-data set lucene-snapshot-revision "$REVISION"
+
+cd "$WORKSPACE"
 
 if [[ "${UPDATE_ES_LUCENE_SNAPSHOT:-}" ]]; then
   .buildkite/scripts/lucene-snapshot/update-es-snapshot.sh
