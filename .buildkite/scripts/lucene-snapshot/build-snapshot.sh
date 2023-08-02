187d49a313a54a57dd3de63126150ad74d3715c4
Brian Seeders <brian.seeders@elastic.co>
2023-08-02 16:59:21 -0400

    Add aws install

--- .buildkite/scripts/lucene-snapshot/build-snapshot.sh
+++ .buildkite/scripts/lucene-snapshot/build-snapshot.sh
@@ -14,6 +14,13 @@ echo "Lucene Revision: $REVISION"
 
 ./gradlew localSettings
 ./gradlew clean mavenToLocal -Dversion.suffix="SNAPSHOT-$REVISION" -Dmaven.repo.local="$(pwd)/build/maven-local"
+
+## TODO move aws install to image build
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm -rf awscliv2.zip aws
+
 aws s3 sync build/maven-local/ "s3://download.elasticsearch.org/lucenesnapshots/$REVISION/" --acl public-read
 
 buildkite-agent meta-data set lucene-snapshot-revision "$REVISION"
