4cf563a431435e281dd289855943bc0ed3713968
Brian Seeders <brian.seeders@elastic.co>
2023-08-02 16:56:24 -0400

    Use old vault for snapshot creds

---
+++ .buildkite/scripts/lucene-snapshot/get-credentials.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# WARNING: this script will echo the credentials to the console. It is meant to be called from another script and captured in a variable.
+# It should really only be used inside .buildkite/hooks/pre-command
+
+VAULT_ROLE_ID=$(vault read -field=role-id secret/ci/elastic-elasticsearch/legacy-vault-credentials)
+VAULT_SECRET_ID=$(vault read -field=secret-id secret/ci/elastic-elasticsearch/legacy-vault-credentials)
+VAULT_ADDR=https://secrets.elastic.co:8200
+VAULT_TOKEN=$(vault write -field=token auth/approle/login role_id=$VAULT_ROLE_ID secret_id=$VAULT_SECRET_ID)
+
+vault read -format=json aws-elastic-ci-prod/creds/lucene-snapshots
