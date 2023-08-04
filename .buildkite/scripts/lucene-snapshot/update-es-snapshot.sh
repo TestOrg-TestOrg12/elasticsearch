c4487423a9ae7ae0fe24c1a303d7ceef975f0ab6
Brian Seeders <brian.seeders@elastic.co>
2023-08-04 14:59:42 -0400

    Move checkout

--- .buildkite/scripts/lucene-snapshot/update-es-snapshot.sh
+++ .buildkite/scripts/lucene-snapshot/update-es-snapshot.sh
@@ -9,6 +9,9 @@ LUCENE_REVISION=SNAPSHOT-$(buildkite-agent meta-data get lucene-snapshot-revisio
 
 echo "Lucene Revision: $LUCENE_REVISION"
 
+git checkout "$BUILDKITE_BRANCH"
+git pull --ff-only origin "$BUILDKITE_BRANCH"
+
 # Replace `lucene = <version>` string in version.properties
 sed -E "s/^(lucene *= *[^ ]*  *).*\$/\1$LUCENE_REVISION/" build-tools-internal/version.properties > new-version.properties
 mv new-version.properties build-tools-internal/version.properties
@@ -18,7 +21,5 @@ git status
 git config --global user.name elasticmachine
 git config --global user.email '15837671+elasticmachine@users.noreply.github.com'
 
-git checkout "$BUILDKITE_BRANCH"
-git pull --ff-only origin "$BUILDKITE_BRANCH"
-git add -u build-tools-internal/version.properties
+git add build-tools-internal/version.properties
 git push origin "$BUILDKITE_BRANCH"
