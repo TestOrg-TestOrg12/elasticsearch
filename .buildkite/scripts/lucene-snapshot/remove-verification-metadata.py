66e0ee5a5bc2e41bd3ac522ab516e6c013041edb
Brian Seeders <brian.seeders@elastic.co>
2023-08-07 17:22:26 -0400

    Update lucene version in docs as well

--- .buildkite/scripts/lucene-snapshot/remove-verification-metadata.py
+++ .buildkite/scripts/lucene-snapshot/remove-verification-metadata.py
@@ -1,13 +1,13 @@
 import re
 
-x = re.compile(
+regex = re.compile(
     r'<component group="org.apache.lucene" name="lucene-.*?</component>\s*',
     re.MULTILINE | re.DOTALL,
 )
 
 with open("gradle/verification-metadata.xml", "r+") as f:
     text = f.read()
-    text = x.sub("", text)
+    text = regex.sub("", text)
     f.seek(0)
     f.truncate()
     f.write(text)
